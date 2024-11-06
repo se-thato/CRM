333a5e2efa40afb3056d455bf7f668f8d7afc265
thatoselepe80@gmail.com <thatoselepe80@gmail.com>
2024-11-06 22:35:27 +0200

    first commit

---
+++ mycrm/mydb.py
@@ -0,0 +1,31 @@
+"""
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    passwd = '@THATO53',
+)
+
+#prepare cursor object
+cursorObject = dataBase.cursor()
+
+#create the database
+cursorObject.execute("CREATE DATABASE dynamic")
+
+print("Done! database created successfully")
+
+
+database = mysql.connector.connect(
+    host = "localhost",
+    user = "root", 
+    passwd = "@THATO53",
+    database = "dynamic"
+
+)
+
+cursorObject = database.cursor()
+
+print("connected to 'dynamic' database successfully!")
+
+"""
