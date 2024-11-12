fee3d7d1f5e8995697fe913a6703f3fb7a99ad74
se-thato <thatoselepe80@gmail.com>
2024-11-12 06:19:05 +0000

    made changes

--- mycrm/crm_app/urls.py
+++ mycrm/crm_app/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     #path('login/', views.login_user, name='login'),
-    #path('', views.logout_user, name='home'),
+    path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register_user, name='register'),
+    path('record/<int:pk>', views.customer_record, name='record'),
 ]
--- mycrm/crm_app/views.py
+++ mycrm/crm_app/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
+from .models import Record
+
 
 def home(request):
+    #showing all the record on my home page
+    records = Record.objects.all()
+
     #check to see if logging in 
     if request.method == "POST":
         username = request.POST['username']
@@ -11,16 +17,49 @@ def home(request):
         user = authenticate(request, username=username, password=password)
         if user is not None :
             login(request, user)
-            messages.success(request, "Ohh yeah!! You have been logged in successfully.")
+            messages.success(request, "Ohh yeah!! You Have Been Logged In Successfully Welcome Back.")
             return redirect('home')
         else:
-            messages.success(request, "Opps!! there was an error logging in, Please try again..")
+            messages.success(request, "Opps!! There Was An Error Logging In, Please Try Again..")
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records': records})
 
 
 
 def logout_user(request):
-    pass 
+    logout(request)
+    messages.success(request, "Ohh No!! You Have Been Logged Out..")
+    return redirect('home')
     
+
+
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #Authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You Have Successfully Registered!!")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form': form})
+
+    return render(request, 'register.html', {'form': form})
+
+
+def customer_record(request, pk):
+    #only logged in user can view the details
+    if request.user.is_authenticated:
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {' customer_record': customer_record})
+    #if they are not logged in 
+    else:
+        messages.success(request, "You Have To Login To View That Page.")
+        return redirect('home')
